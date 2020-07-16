1c6592f5ce861d311c6f5e2a8f4c2fcc39a3df0e
Michael Chang <b02605061@gmail.com>
2020-07-15 17:03:41 -0700

    starting hw2

---
+++ hw2_neural_networks/hw2.py
@@ -0,0 +1,1 @@
+import numpy as np                          # Load the numpy libraries with alias 'np' 
---
+++ hw2_neural_networks/neural_net.py
@@ -0,0 +1,3 @@
+import numpy as np                          # Load the numpy libraries with alias 'np' 
+
+
